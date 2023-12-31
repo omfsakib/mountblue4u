48b986662155532b77bd2a18d3b8f5fef7cc9c5d
omfsakib <shakib1lk@gmail.com>
2023-12-31 13:05:53 +0600

    Home page design

--- apps/preference/context_processors.py
+++ apps/preference/context_processors.py
@@ -6,7 +6,9 @@ def get_context(request):
     menus = MenuModel.objects.filter(is_active=True)
     context = {
         'preference': preference,
-        'main_menu': menus.filter(type=MenuModel.MenuType.main)
+        'main_menu': menus.filter(type=MenuModel.MenuType.main),
+        'footer_menu1': menus.filter(type=MenuModel.MenuType.footer1),
+        'footer_menu2': menus.filter(type=MenuModel.MenuType.footer2),
     }
 
     return context
